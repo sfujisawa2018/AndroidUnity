98168d8c22b4a32a54f36ea0497b5a285d8f2d4a
fujisawa <sfujisasa2018@gmail.com>
2018-11-14 10:14:10 +0900

    11/14 マルチタッチ情報まで

---
+++ Assets/TouchInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchInput : MonoBehaviour {
+
+    private Text textComponent;
+
+    // Use this for initialization
+    void Start () {
+        textComponent = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // 一か所以上のタッチ情報あり？
+        if (Input.touchCount > 0)
+        {
+            string text = "";
+            foreach (Touch touch in Input.touches)
+            {
+                text += "touch" + touch.fingerId +
+                    ":(" + touch.position.x + "," + touch.position.y + ")" + "\n";
+            }
+            textComponent.text = text;
+        }
+        else
+        {
+            textComponent.text = "No touch";
+        }
+
+    }
+}
