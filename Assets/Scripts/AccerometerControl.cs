69b7dffab00c292610f5503f97565d1a3fc4f4a9
fujisawa <sfujisawa2016@gmail.com>
2019-01-24 12:53:57 +0900

    加速度センサー途中まで

---
+++ Assets/Scripts/AccerometerControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccerometerControl : MonoBehaviour {
+
+    // 加速度
+    //public Vector3 acceleration;
+	
+	// Update is called once per frame
+	void Update () {
+        //acceleration = Input.acceleration;
+    }
+}
